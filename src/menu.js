6026881adac873dcb6cfab56d16951e1b38b4c51
pxai <pello.altadill@gmail.com>
2021-07-14 09:40:00 +0200

    last changes

--- src/menu.js
+++ src/menu.js
@@ -11,18 +11,39 @@ class Menu extends Phaser.Scene {
     create(){
         console.log("menu create");
         this.options = ["one player", "two player", "settings", "help"];
+        this.scenes = ["single_player_game", "multi_player_game", "settings", "help"];
+        this.current = 0;
 
         for(let i = 0;i < 4; i++){
             this.add.bitmapText(40, 40 + (50 * i), "squareFont",this.options[i], 36);
         }
-       
         this.ENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+        this.input.keyboard.on('keydown-UP', this.menuUp, this);
+        this.input.keyboard.on('keydown-DOWN', this.menuDown, this);
     }
 
     update(){
         if(this.ENTER.isDown) {
-            this.scene.start("single_player_game");
+            this.scene.start(this.scenes [this.current]);
           } 
+    }
+
+    menuUp(){
+        if(this.current === 0){
+            this.current = this.options.length -1;
+        }else{
+            this.current--;
+        }
+        console.log(this.options[this.current], this.current);
+    }
+
+    menuDown(){
+        if(this.current === this.options.length -1){
+            this.current = 0;
+        }else{
+            this.current++;
+        }
+        console.log(this.options[this.current], this.current);
     }
 }
 
