59945a68b0e2781d623d2261925ab5f9cdf8ce73
pxai <pello.altadill@gmail.com>
2021-07-25 10:52:21 +0200

    Added settings

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,5 @@
+import settingsOptions from './settingsOptions';
+
 class Settings extends Phaser.Scene {
     constructor(){
         super({key: "settings"});
@@ -9,8 +11,16 @@ class Settings extends Phaser.Scene {
     }
 
     create(){
+        this.width = this.sys.game.config.width;
+        this.height = this.sys.game.config.height;
+        this.center_width = this.width/2;
+        const center_height = this.height/2;
+        this.add.bitmapText(this.center_width, 50 , "squareFont","ARKAPONG", 60).setOrigin(0.5);
+        this.add.bitmapText(this.center_width, 110 , "squareFont","SETTINGS", 40).setOrigin(0.5);
         console.log("settings create");
-        this.add.bitmapText(40, 60, "squareFont","hello settings ", 36);
+        for(let i = 0; i < settingsOptions.length; i++){
+            this.add.bitmapText(this.center_width, 110 , "squareFont",settingsOptions[i].name, 30).setOrigin(0.5);
+        }
         this.ESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
     }
 
---
+++ src/settings_options.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        name: "Ball speed",
+        options: [1,2,3,4,5],
+        description: "set speed of the balls"
+    },
+    {
+        name: "Paddle speed",
+        options: [1,2,3,4,5],
+        description: "speed of the paddles"
+    },
+    {
+        name: "Win score",
+        options: [1,2,3,4,5],
+        description: "set score to win"
+    },
+];
