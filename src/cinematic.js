fa380686afc130056c59f6c20c0f4e075a2a1394
pxai <pello.altadill@gmail.com>
2021-07-30 19:35:56 +0200

    Sea effect

--- src/cinematic.js
+++ src/cinematic.js
@@ -17,23 +17,66 @@ class Cinematic extends Phaser.Scene {
         this.height = this.sys.game.config.height;
         this.center_width = this.width/2;
         const center_height = this.height/2;
+        this.sea_height = center_height+100;
         //this.add.bitmapText(this.center_width, 150 , "squareFont","ARKAPONG", 160).setOrigin(0.5);
         this.ENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
         this.blockCreator = new BlockCreator(this);
         //this.blockCreator.wall();
         this.izquierda = new Palas(this, 200,this.height-20, "izquierda")
-       
         this.derecha = new Palas(this,600,this.height-20, "derecha");
         this.add.rectangle(0,this.height-5,this.width,6,0xffffff).setOrigin(0);
         this.add.rectangle(700, 200, 50, 50, 0xffffff)
-        this.add.rectangle(0,center_height+100, this.width, 1, 0xffffff).setOrigin(0);
-        this.ball = this.add.rectangle(this.center_width, 450, 10, 10, 0xffffff)
+        this.add.rectangle(0,this.sea_height, this.width, 2, 0xffffff).setOrigin(0);
+        this.createBall();
+        this.waves = [];
+        this.generateWaves();
+        setTimeout(() => this.blockCreator.wall(), 3000);
     }
 
     update(){
         if(this.ENTER.isDown) {
             this.scene.start("menu");
-          }
+        }
+
+        this.waves.forEach((wave, i) => this.updateWave(wave, i));
+    }
+
+    createBall(velocityX = -180){
+        this.ball = this.physics.add.image(this.center_width, this.height - 40, "ball");
+        this.ball.x = this.center_width;
+        this.ball.setVelocityX(Phaser.Math.Between(0,1) ? 100 : -100);
+        this.ball.setCollideWorldBounds(true);
+        this.ball.setBounce(1);
+        this.physics.add.collider(this.ball, this.izquierda, this.chocaPala, null, this);
+        this.physics.add.collider(this.ball, this.derecha, this.chocaPala, null, this);
+    }
+
+    chocaPala (ball, paddle) {
+        const jump = Phaser.Math.Between(5,20);
+        paddle.body.y -= jump;
+        setTimeout(() => this.goBack(paddle, jump), 100);
+    }
+
+    goBack (paddle, jump) {
+        paddle.y += jump;
+    }
+
+    generateWaves () {
+        this.waves.push(this.add.rectangle(0,this.sea_height + 2, this.width, 1, 0xffffff).setOrigin(0));
+    }
+
+    updateWave (wave, i) {
+        if (wave.y > this.sea_height + Phaser.Math.Between(40,45)) {
+            wave.destroy();
+            this.waves[i] = null;
+        } else if(wave.y > this.sea_height + 30 && this.waves.length < 2) {
+            this.generateWaves();
+        } else {
+            wave.y += Phaser.Math.Between(0,1);
+        }
+
+        this.waves = this.waves.filter(wave => wave !== null);
+        if (this.waves.length < 1) this.generateWaves();
     }
 }
 
